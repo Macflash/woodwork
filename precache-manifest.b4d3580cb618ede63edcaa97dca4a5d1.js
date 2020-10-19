50165b1cdc63cf85acd6e27f281b510b07f8f6f8
Macflash <the.alex.breyfogle@gmail.com>
2020-10-18 20:16:26 -0700

    Updates

---
+++ precache-manifest.b4d3580cb618ede63edcaa97dca4a5d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e17e3809df279f4c42f364cca4e8d9cb",
+    "url": "/woodowrk/index.html"
+  },
+  {
+    "revision": "bc83c5132f9ddf945798",
+    "url": "/woodowrk/static/css/main.14dad1a1.chunk.css"
+  },
+  {
+    "revision": "3067aed17cc121205458",
+    "url": "/woodowrk/static/js/2.5c985703.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/woodowrk/static/js/2.5c985703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc83c5132f9ddf945798",
+    "url": "/woodowrk/static/js/main.403b7067.chunk.js"
+  },
+  {
+    "revision": "a7b96259665177b4118b",
+    "url": "/woodowrk/static/js/runtime-main.fb5df4d9.js"
+  }
+]);
